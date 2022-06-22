897f3cb3dd802540673fb3b97cf2f61b448f2bbb
Alireza Nematolahy <swmn_nmt@icloud.com>
2022-06-22 12:00:58 +0430

    MySQL database has been integrated

--- My_Online_Shop/settings.py
+++ My_Online_Shop/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'My_Online_Shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MyOnlineShop',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '91827376491@Anem',
     }
 }
 
