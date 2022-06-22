1829ed9096947495e2d425c578effe6fb9ea0978
Alireza Nematolahy <swmn_nmt@icloud.com>
2022-06-22 12:16:54 +0430

    Category and Product models have been registered on the administration site.

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    # Remember that you use the prepopulated_fields attribute to specify fields where
+    # the value is automatically set using the value of other fields. As you have seen
+    # before, this is convenient for generating slugs.
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
